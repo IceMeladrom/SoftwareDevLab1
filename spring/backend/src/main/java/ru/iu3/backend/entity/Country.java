58f69c2de784a9b9c1bafc4f68652e0358fd9a8b
Robert <robertkryt@gmail.com>
2025-04-12 17:32:00 +0300

    Lab 5 and Lab 6 final

---
+++ spring/backend/src/main/java/ru/iu3/backend/entity/Country.java
@@ -0,0 +1,26 @@
+package ru.iu3.backend.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "countries")
+@Access(AccessType.FIELD)
+public class Country {
+    public Country() { }
+    public Country(Long id) {
+        this.id = id;
+    }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    public long id;
+    @Column(name = "name", nullable = false, unique = true)
+    public String name;
+    @JsonIgnore
+    @OneToMany(mappedBy = "country", targetEntity = Artist.class)
+    public List artists = new ArrayList();
+}
