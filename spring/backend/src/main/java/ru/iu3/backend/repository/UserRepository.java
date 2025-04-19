52dba0f63092beb80deeddce7a99917fd9d0e64a
Robert <robertkryt@gmail.com>
2025-04-19 19:48:00 +0300

    Lab 7 final

--- spring/backend/src/main/java/ru/iu3/backend/repository/UserRepository.java
+++ spring/backend/src/main/java/ru/iu3/backend/repository/UserRepository.java
@@ -2,9 +2,13 @@ package ru.iu3.backend.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import ru.iu3.backend.entity.Artist;
 import ru.iu3.backend.entity.User;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByToken(String token);
+
+    Optional<User> findByLogin(String login);
 }
